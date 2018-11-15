be35265d94910dfe0688113d09848313bfbc20d3
wellwind <wellwind@mail2000.com.tw>
2018-11-15 21:46:46 +0800

    允許關閉 dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Overlay, OverlayConfig } from '@angular/cdk/overlay';
-import { ComponentPortal } from '@angular/cdk/portal';
-import { Component } from '@angular/core';
+import { Overlay, OverlayConfig, OverlayRef } from '@angular/cdk/overlay';
+import { ComponentPortal, PortalInjector } from '@angular/cdk/portal';
+import { Component, Injector } from '@angular/core';
 import { DialogComponent } from './dialog/dialog.component';
 
 @Component({
@@ -9,7 +9,7 @@ import { DialogComponent } from './dialog/dialog.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private overlay: Overlay) {}
+  constructor(private overlay: Overlay, private injector: Injector) {}
 
   showDialog() {
     const strategy = this.overlay
@@ -25,6 +25,12 @@ export class AppComponent {
     });
 
     const overlayRef = this.overlay.create(config);
-    overlayRef.attach(new ComponentPortal(DialogComponent));
+
+    const injector = new PortalInjector(
+      this.injector,
+      new WeakMap<any, any>([[OverlayRef, overlayRef]])
+    );
+
+    overlayRef.attach(new ComponentPortal(DialogComponent, null, injector));
   }
 }
--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,3 +1,4 @@
+import { OverlayRef } from '@angular/cdk/overlay';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(private overlayRef: OverlayRef) { }
 
   ngOnInit() {
   }
 
   close() {
-
+    this.overlayRef.detach();
   }
 
 }
